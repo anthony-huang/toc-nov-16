c92b318a9eb6e7f850a94ef2c79bd766c6a80bf2
Pusaka Kaleb Setyabudi <sokokaleb@gmail.com>
2016-11-13 17:06:11 +0700

    Update runner to MAXLEN=200000

--- typeracing/runner.cpp
+++ typeracing/runner.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using namespace tcframe;
 
-const int MAX_LEN = 1000000;
+const int MAX_LEN = 200000;
 const string SIGMA = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 class Problem : public BaseProblem {
@@ -51,12 +51,16 @@ protected:
 
     void SampleTestCases() {
         SAMPLE_CASE({
-            "sayamaksaysayamakannasi"
+            "sayamakannasisayamaksay"
         }, {1});
         SAMPLE_CASE({
             "aaaaaa"
         }, {1});
     }
+
+	inline char ChooseOne(const string& s) {
+		return s[rnd.nextInt(s.length())];
+	}
 
 	void Random(const int length, string& s, const string& SIGMA) {
 		s = "";
@@ -93,6 +97,64 @@ protected:
 		return ;
 	}
 
+	void RandomThenRandomChange(const int length, const int longest_length, string& s, const string& SIGMA) {
+		s = "";
+		string utama = "";
+		for (int i = 0; i < longest_length; ++i) {
+			utama.push_back(ChooseOne(SIGMA));
+		}
+		rnd.shuffle(utama.begin(), utama.end());
+
+		vector<string> vec;
+		vec.clear();
+		vec.push_back(utama);
+		int total_length = longest_length;
+		while (total_length < length) {
+			utama[utama.length() - 1] = ChooseOne(SIGMA);
+			vec.push_back(utama);
+			total_length += utama.length();
+		}
+		if (total_length > length) vec.pop_back();
+		rnd.shuffle(vec.begin(), vec.end());
+		for (const auto& ss : vec) {
+			s.append(ss);
+		}
+
+		return ;
+	}
+
+	void RandomIncreasing(const int length, string& s, const string& SIGMA) {
+		string utama = "";
+		s = "";
+		for (int i = 0; i < length; ++i) {
+			utama.push_back(ChooseOne(SIGMA));
+			s.append(utama);
+		}
+
+		return ;
+	}
+
+	void RandomDecreasing(const int length, string& s, const string& SIGMA, const bool chg = false) {
+		string utama = "";
+		for (int i = 0; i < length; ++i) {
+			utama.push_back(ChooseOne(SIGMA));
+		}
+
+		s = "";
+		for (int i = 0; i < length - 1; ++i) {
+			utama[utama.length() - 1] = ChooseOne(SIGMA);
+			s.append(utama);
+			utama.pop_back();
+		}
+		utama[utama.length() - 1] = ChooseOne(SIGMA);
+		s.append(utama);
+
+		return ;
+	}
+
+	const vector<int> UTAMA_LENGTHS = {1000, 5000, 10000, 50000, 100000, 200000};
+	const vector<int> UTAMA_DEC_LENGTHS = {600, 500, 400};
+
 	void TestGroup1() {
 		assignToSubtasks({1});
 
@@ -101,7 +163,7 @@ protected:
 		CASE(s = "A");
 		CASE(s = "aAaAaAaAaA");
 		CASE(s = "aAaAaAaAa");
-		
+
 		CASE(Random(MAX_LEN, s, "a"));
 		CASE(Random(MAX_LEN, s, "0"));
 		CASE(Random(MAX_LEN, s, "A"));
@@ -112,53 +174,33 @@ protected:
 		CASE(Random(MAX_LEN, s, "0Aa"));
 		CASE(Random(MAX_LEN, s, "0Aa"));
 
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
-		CASE(Random(MAX_LEN, s, SIGMA));
+		for (const auto& length : UTAMA_LENGTHS) {
+			for (int multiplicity = 1; multiplicity <= 5; ++multiplicity) {
+				CASE(RandomThenRandomChange(MAX_LEN, length, s, SIGMA));
+			}
+		}
 
-		CASE(RandomPrefix(MAX_LEN, 100, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 100, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 100, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 100, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 500, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 500, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 500, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 500, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 1000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 1000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 1000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 1000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 5000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 5000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 5000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 5000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 10000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 10000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 10000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 10000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 50000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 50000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 50000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 50000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 100000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 100000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 100000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 100000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 500000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 500000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 500000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, 500000, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, MAX_LEN, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, MAX_LEN, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, MAX_LEN, s, SIGMA));
-		CASE(RandomPrefix(MAX_LEN, MAX_LEN, s, SIGMA));
+		for (const auto& length : UTAMA_DEC_LENGTHS) {
+			for (int multiplicity = 1; multiplicity <= 5; ++multiplicity) {
+				CASE(RandomIncreasing(length, s, SIGMA));
+			}
+		}
+
+		for (const auto& length : UTAMA_DEC_LENGTHS) {
+			for (int multiplicity = 1; multiplicity <= 5; ++multiplicity) {
+				CASE(RandomDecreasing(length, s, SIGMA));
+			}
+		}
+
+		for (int multiplicity = 1; multiplicity <= 5; ++multiplicity) {
+			CASE(Random(MAX_LEN, s, SIGMA));
+		}
+
+		for (const auto& length : UTAMA_LENGTHS) {
+			for (int multiplicity = 1; multiplicity <= 10; ++multiplicity) {
+				CASE(RandomPrefix(MAX_LEN, length, s, SIGMA));
+			}
+		}
 	}
 };
 
